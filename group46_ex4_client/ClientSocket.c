09a49063ff26c637bfa74d3e810bf811d8b44005
Anton <Anton@ANTON-MSI>
2018-01-13 13:10:56 +0200

    !!!

--- group46_ex4_client/ClientSocket.c
+++ group46_ex4_client/ClientSocket.c
@@ -9,18 +9,21 @@ Last updated by Amnon Drory, Winter 2011.
 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 
-
 #include <winsock2.h>
 #include <stdio.h>
 #include <string.h>
 #include "SocketShared.h"
 #include "SendRecvTools.h"
 
-/*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
 
+/*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
+char SendStr[256];
+static HANDLE h_message;
+static HANDLE h_input;
 static void ReportErrorAndEndProgram();
 int PrintToLogFile(char *p_msg, char *path);
 SOCKET m_socket;
+static char *send_char;
 
 /*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
 
@@ -58,27 +61,26 @@ static DWORD RecvDataThread(void)
 /*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
 
 //Sending data to the server
-static DWORD SendDataThread(LPVOID lpParam)
+static DWORD MsgThread(LPVOID lpParam)
 {
-	char SendStr[256];
+	DWORD wait_res;
+	BOOL release_res;
 	TransferResult_t SendRes;
-
+	// The first operation of the client is to send a message that contains the username of the client,
+	//so it happens automatically as the message thread starts up ****************************************
 	char *username;
 	username = (char *)lpParam;
 	SendRes = SendString(username, m_socket);
-
 	if (SendRes == TRNS_FAILED)
 	{
 		printf("Socket error while trying to write data to socket\n");
 		return 0x555;
 	}
-
-	while (1)
-	{	
-		gets_s(SendStr, sizeof(SendStr)); //Reading a string from the keyboard
-
-		if (STRINGS_ARE_EQUAL(SendStr, "quit"))
-			return 0x555; //"quit" signals an exit from the client side
+	// ***************************************************************************************************
+	while (1) 
+	{
+		wait_res = WaitForSingleObject(h_message, INFINITE);
+		if (wait_res != WAIT_OBJECT_0) ReportErrorAndEndProgram();
 
 		SendRes = SendString(SendStr, m_socket);
 
@@ -87,6 +89,35 @@ static DWORD SendDataThread(LPVOID lpParam)
 			printf("Socket error while trying to write data to socket\n");
 			return 0x555;
 		}
+
+		release_res = ReleaseSemaphore(
+			h_input,
+			1, 		
+			NULL);
+		if (release_res == FALSE) ReportErrorAndEndProgram();
+	}
+}
+
+/*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
+
+//Sending data to the server
+static DWORD SendDataThread(void)
+{
+	DWORD wait_res;
+	BOOL release_res;
+
+	while (1)
+	{	
+		gets_s(SendStr, sizeof(SendStr)); //Reading a string from the keyboard
+
+		release_res = ReleaseSemaphore(
+			h_message,
+			1, 		/* Signal that exactly one cell was filled */
+			NULL);
+		if (release_res == FALSE) ReportErrorAndEndProgram();
+
+		wait_res = WaitForSingleObject(h_input, INFINITE);
+		if (wait_res != WAIT_OBJECT_0) ReportErrorAndEndProgram();
 	}
 }
 
@@ -95,7 +126,7 @@ static DWORD SendDataThread(LPVOID lpParam)
 void MainClient(char *path, char *server_ip, char *server_port_char, char *username)
 {
 	SOCKADDR_IN clientService;
-	HANDLE hThread[2];
+	HANDLE hThread[3];
 
 	// Initialize Winsock.
 	WSADATA wsaData; //Create a WSADATA object called wsaData.
@@ -163,24 +194,27 @@ void MainClient(char *path, char *server_ip, char *server_port_char, char *usern
 
 	}
 
-	// Send and receive data.
-	/*
-	In this code, two integers are used to keep track of the number of bytes that are sent and received.
-	The send and recv functions both return an integer value of the number of bytes sent or received,
-	respectively, or an error. Each function also takes the same parameters:
-	the active socket, a char buffer, the number of bytes to send or receive, and any flags to use.
 
-	*/
+	/* Create two semaphores in order to synchronize two threads:
+	SendDataThread: gets the input from the users keyboard and stores it in a global variable.
+	MsgThread: takes the users' input, then generates and sends the appropriate message to the server.
+	We synchronzie the threads in such a way that MsgThread starts taking the info fron the gloabl variable only after SendDataThread 
+	had signaled that it has stored the info. */
 
+	h_message = CreateSemaphore(
+		NULL,	
+		0,		
+		1,		
+		NULL); 
+	h_input = CreateSemaphore(
+		NULL,	
+		0,		
+		1,		
+		NULL); 
+
+	/* The three threads we were asked to create:
+	As mentioned above, including: RecvDataThread - A thread that recieves data from the server 	*/
 	hThread[0] = CreateThread(
-		NULL,
-		0,
-		(LPTHREAD_START_ROUTINE)SendDataThread,
-		username,
-		0,
-		NULL
-	);
-	hThread[1] = CreateThread(
 		NULL,
 		0,
 		(LPTHREAD_START_ROUTINE)RecvDataThread,
@@ -189,13 +223,33 @@ void MainClient(char *path, char *server_ip, char *server_port_char, char *usern
 		NULL
 	);
 
+	hThread[1] = CreateThread(
+		NULL,
+		0,
+		(LPTHREAD_START_ROUTINE)MsgThread,
+		username,
+		0,
+		NULL
+	);
+	hThread[2] = CreateThread(
+		NULL,
+		0,
+		(LPTHREAD_START_ROUTINE)SendDataThread,
+		NULL,
+		0,
+		NULL
+	);
+
+
 	WaitForMultipleObjects(2, hThread, FALSE, INFINITE);
 
 	TerminateThread(hThread[0], 0x555);
 	TerminateThread(hThread[1], 0x555);
+	TerminateThread(hThread[2], 0x555);
 
 	CloseHandle(hThread[0]);
 	CloseHandle(hThread[1]);
+	CloseHandle(hThread[2]);
 
 	closesocket(m_socket);
 
