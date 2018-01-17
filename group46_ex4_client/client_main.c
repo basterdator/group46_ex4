84cd86f33603cfd7aa790ffbe753ad5419c44b41
Anton <Anton@ANTON-MSI>
2018-01-17 02:32:04 +0200

    Not finished

--- group46_ex4_client/client_main.c
+++ group46_ex4_client/client_main.c
@@ -14,23 +14,21 @@ int main(int argc, char *argv[])
 		retval = fopen_s(&p_stream, argv[1], "w");
 		if (0 != retval)
 		{
-			printf("Failed to open file.\n");
+			PrintToLogFile("Custom message: Failed to open the file.\n", argv[1]);
 			return -1;
 		}
 		retval = fputs("\0", p_stream);
 		retval = fclose(p_stream);
 		if (0 != retval)
 		{
-			printf("Failed to close file.\n");
+			PrintToLogFile("Custom message: Failed to clear the file.\n", argv[1]);
 			return -1;
 		}
-		// ====================================================================
+		// =====================================================================
 
 		res = MainClient(argv[1], argv[2], argv[3], argv[4]);
-		printf("Return code %d", res);
+		//printf("Return code %d", res);
 		return res;
-
-
 	}
 	else
 	{
